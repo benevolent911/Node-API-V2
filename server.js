c1ad68db30147863d7ffa6e0a1ae236dd7c926e8
Erick Rebadeo <erick.rebadeo@ffufm.com>
2024-02-29 21:28:15 +0800

    Add cors

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const productRoute = require("./routes/productRoute");
 const errorMiddleware = require("./middleware/errorMiddleware");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
