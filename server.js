17fd3b87f4d7cadd85c86adc2a96fb9076dfb8ce
Erick Rebadeo <erick.rebadeo@ffufm.com>
2024-02-29 18:36:55 +0800

    Install nodemon and mongoose

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 //routes
@@ -6,6 +7,20 @@ app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
 
-app.listen(3000, () => {
-  console.log("Node API app is running on port 3000");
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:1qaz2wsx@cluster0api.szjncpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0API"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Node API app is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
