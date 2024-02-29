15afc729c9b878d53ff3ef395486ee4ee23d4b3f
Erick Rebadeo <erick.rebadeo@ffufm.com>
2024-02-29 20:45:37 +0800

    Add env, routes and product controller

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/productModel");
+const productRoute = require("./routes/productRoute");
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//routes
+app.use("/api/products", productRoute);
+
 app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
@@ -15,76 +20,12 @@ app.get("/blog", (req, res) => {
   res.send("Hello Blog");
 });
 
-app.get("/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post("/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.put("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `Cannot find product with ID ${id}` });
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.delete("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findOneAndDelete(id);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `Cannot find product with ID ${id}` });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
 mongoose
-  .connect(
-    "mongodb+srv://admin:1qaz2wsx@cluster0api.szjncpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0API"
-  )
+  .connect(MONGO_URL)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(3000, () => {
-      console.log("Node API app is running on port 3000");
+    app.listen(PORT, () => {
+      console.log(`Node API app is running on port ${PORT}`);
     });
   })
   .catch((error) => {
